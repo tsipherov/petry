535f3c67c54bb57bed23d6c34a45ab94d4aabf30
Tsiferov <tsiferov@ukr.net>
2020-12-23 23:02:25 +0200

    added props and handler like and important

--- src/components/postItem/PostItem.jsx
+++ src/components/postItem/PostItem.jsx
@@ -2,20 +2,50 @@ import React from "react";
 import "./post-list-item.css";
 
 class PostItem extends React.Component {
+
+  state = {
+    important: this.props.important,
+    like: false
+  }
+
+  handlerImportant = () => { 
+    this.setState((prevState) => { 
+      return {
+        important: !prevState.important
+      }
+    })
+  }
+  
+  handlerLike = () => { 
+    this.setState(({ like }) => { 
+      return {
+        like: !like
+      }
+    })
+  }
+
   render() {
-    const { important, label } = this.props;
+    const { label } = this.props;
+    const { important, like } = this.state
 
     let classes = "d-flex justify-content-between app-list-item";
 
     if (important) {
       classes += " important";
     }
-
+    
+    if (like) {
+      classes += " like";
+    }
     return (
       <div className={classes}>
-        <span className="app-list-item-label">{label}</span>
+        <span className="app-list-item-label"
+          onClick={this.handlerLike }
+        >{label}</span>
         <div className="d-flex justify-content-center align-items-center">
-          <button type="button" className="btn-star btn-sm">
+          <button type="button"
+            onClick={ this.handlerImportant}
+            className="btn-star btn-sm">
             <i className="fa fa-star"></i>
           </button>
           <button type="button" className="btn-trash btn-sm">
